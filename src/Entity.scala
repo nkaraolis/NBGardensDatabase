7780a4a80884b908c7df8701666c2c485b2427c0
Luke's a noob <nick.karaolis@qa.com>
2016-06-29 15:14:11 +0100

    More shit fuckfaces

--- src/Entity.scala
+++ src/Entity.scala
@@ -5,24 +5,30 @@ import scala.collection.mutable
 /**
   * Created by Nick on 28/06/2016.
   */
+//Superclass for the database tables
 trait Entity {
 
+  //Variables required for database connection
   val url = "jdbc:mysql://localhost:3306/nbgardensv0.2?autoReconnect=true&useSSL=false"
   val driver = "com.mysql.jdbc.Driver"
   val username = "root"
   val password = "password"
   var connection: Connection = _
 
+  //Table names, column names and types which are set differently for the different tables
   var dbTableName : String
   var dbTableColumns : mutable.Queue[String]
   var dbTableTypes: mutable.Queue[String]
 
+  //Parameters required for object creation in tables
   var stringParams = new mutable.Queue[String]
   var intParams = new mutable.Queue[Int]
   var doubleParams = new mutable.Queue[Double]
 
+  //Implemented in subclasses to populate their collections
   def createEntity(): Unit = {}
 
+  //Gets the data from the database for the required table and passes the data to add to the collections
   def populateEntity(entity: Entity): Unit = {
     try {
       Class.forName(driver)
--- src/Order.scala
+++ src/Order.scala
@@ -5,22 +5,29 @@
 import java.sql.{Connection, DriverManager}
 import scala.collection.mutable.Queue
 
+//Represents customer orders in the system
 object Order extends Entity {
 
+  //Set the table name to be orders
   var dbTableName = "orders"
 
+  //Column names for the orders table
   override var dbTableColumns: Queue[String] = new Queue[String]
   dbTableColumns +=("OrderID", "Date", "Price", "CustomerName", "CustomerID", "Address", "Status", "StaffAssigned")
 
+  //Column types for the orders table
   override var dbTableTypes: Queue[String] = new Queue[String]
   dbTableTypes +=("string", "string", "double", "string", "string", "string", "string", "string")
 
+  //Collection of orders
   var orders = new Queue[OrderDescription]
 
+  //Case class to represent one order
   case class OrderDescription(orderID: String, date: String, price: Double, customerName: String, customerID: String, address: String, status: String, staff: String) extends EntityDescription {
 
   }
 
+  //Create a new order and add it to the collection of orders
   override def createEntity(): Unit = {
     var addNewOrder = new OrderDescription(stringParams(0), stringParams(1), doubleParams(0), stringParams(2), stringParams(3), stringParams(4), stringParams(5), stringParams(6))
     orders += addNewOrder
